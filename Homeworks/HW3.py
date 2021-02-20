12b5dac0172d99c94de7468f84de2bffa10881f1
Serhat ERENEL <56649135+srhternl@users.noreply.github.com>
2021-02-20 17:22:04 +0300

    Add files via upload

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -4,7 +4,7 @@ def prime_first(number): # asal sayı hesaplayan 1. fonksiyonum.
             if number % i == 0:
                 break
         else:
-            print("Between 0-500 prime numbers:", number)
+            print("Prime numbers between 0-500:", number)
 
 def prime_second(number): # asal sayı hesaplayan 2. fonksiyonum.
     if number !=1 and number !=0:
@@ -12,7 +12,7 @@ def prime_second(number): # asal sayı hesaplayan 2. fonksiyonum.
             if number % i == 0:
                 break
         else:
-            print("Between 500-1000 prime numbers:", number)
+            print("Prime numbers between 500-1000:", number)
 
 for i in list(range(1000)): # 0-500 ve 500-1000 arası asal sayıları hesaplayıp, ekrana yazdıran döngüm.
     if i < 500:
